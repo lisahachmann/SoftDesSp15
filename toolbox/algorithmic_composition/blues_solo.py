8685073ccb4251532bcc5c207fec1db94b37637a
li <lisa.hachmann@gmail.com>
2015-03-26 01:29:34 -0400

    Playing multiple licks

--- toolbox/algorithmic_composition/blues_solo.py
+++ toolbox/algorithmic_composition/blues_solo.py
@@ -23,14 +23,29 @@ def add_note(out, instr, key_num, duration, bpm, volume):
 sampling_rate = 44100.0
 Wavefile.setDefaults(sampling_rate, 16)
 
+blues_scale = [25, 28, 30, 31, 32, 35, 37, 40, 42, 43, 44, 47, 49, 52, 54, 55, 56, 59, 61]
+beats_per_minute = 45               # Let's make a slow blues solo
 bass = GuitarBass(sampling_rate)	# use a guitar bass as the instrument
 solo = AudioStream(sampling_rate, 1)
+curr_note = 0
+add_note(solo, bass, blues_scale[curr_note], 1.0, beats_per_minute, 1.0)
 
-""" these are the piano key numbers for a 3 octave blues scale in A
-	See: http://en.wikipedia.org/wiki/Blues_scale """
-blues_scale = [25, 28, 30, 31, 32, 35, 37, 40, 42, 43, 44, 47, 49, 52, 54, 55, 56, 59, 61]
-beats_per_minute = 45				# Let's make a slow blues solo
+licks = [ [ [1,0.5], [1,0.5], [1, 0.5], [1, 0.5], [-1, 0.5], [-1, 0.5], [-1, 0.5], [-1, 0.5] ] ]
 
-add_note(solo, bass, blues_scale[0], 1.0, beats_per_minute, 1.0)
+for i in range(4):
+    lick = licks[0]
+    for note in lick:
+        curr_note += note[0]
+        if curr_note< 0:
+            curr_note = abs(curr_note)
+        if curr_note >= (len(blues_scale) -1):
+            curr_note = len(blues_scale) - 1
+        add_note(solo, bass, blues_scale[curr_note], note[1], beats_per_minute, 1.0)
+
+#""" these are the piano key numbers for a 3 octave blues scale in A
+#	See: http://en.wikipedia.org/wiki/Blues_scale """
+
+
+#add_note(solo, bass, blues_scale[0], 5.0, beats_per_minute, 15.0)
 
 solo >> "blues_solo.wav"
